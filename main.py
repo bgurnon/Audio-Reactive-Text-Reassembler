5609c3dd7954ddeb397fbb69acaf675862620754
bgurnon <98366458+bgurnon@users.noreply.github.com>
2022-04-04 18:19:24 -0400

    more experimenting on poetryproject

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import KarlTextParser
+import random
 
 with open('textfile.txt', 'r') as f:
     strip = f.read().replace('\n', '')
     print(strip)
 
-import random
-
 with open('textfile.txt') as f:
     lines = f.readlines()
-    print(random.choice(lines))
+    rand_line = random.randint(0, len(lines) - 1)
+    print(lines[rand_line])
 
 
