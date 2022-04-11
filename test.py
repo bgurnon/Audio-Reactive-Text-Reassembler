14cdd9399f5680039317e9d54b918817ec92681d
bgurnon <98366458+bgurnon@users.noreply.github.com>
2022-04-11 03:07:49 -0400

    this shit work g

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import random
 import parser
 
 with open('textfile.txt', 'r+') as f:
+
     paragraph = f.read().replace('\n',' ')
-    string = find_sentences(paragraph)
+    string = parser.find_sentences(paragraph)
     print(random.choice(string))
 
